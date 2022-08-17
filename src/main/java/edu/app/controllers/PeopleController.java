d9f0ad4f5514ed2317b53f81c8e9797ddbe53ccf
Alexandr Mizev <mizzzev@outlook.com>
2022-08-17 16:42:51 +0300

    -two views -added several methods in PersonController (getConcretePersonPage, getNewPersonPage, getPeopleMainPage, annNewPerson)

--- src/main/java/edu/app/controllers/PeopleController.java
+++ src/main/java/edu/app/controllers/PeopleController.java
@@ -1,15 +1,48 @@
 package edu.app.controllers;
 
+import edu.app.models.Person;
+import edu.app.services.PersonService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    private final PersonService personService;
+
+    public PeopleController(PersonService personService) {
+        this.personService = personService;
+    }
+
+
     @GetMapping("/")
-    public String getPeopleMainPage(){
+    public String getPeopleMainPage(Model model) {
+        model.addAttribute("people", personService.getAllPeople());
         return "people/peopleMainPage";
+    }
+    @GetMapping("/{id}")
+    public String getConcretePersonPage(@PathVariable("id") int id,Model model){
+        model.addAttribute("person",personService.getConcretePerson(id));
+        return "people/concretePerson";
+    }
+
+    @GetMapping("/new")
+    public String getNewPersonPage(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/newPerson";
+    }
+
+    @PostMapping
+    public String annNewPerson(@ModelAttribute("person") @Valid Person person, BindingResult errors) {
+        if (errors.hasErrors()) {
+            return "people/newPerson";
+        }
+        personService.addNewPerson(person);
+        return "redirect:/people/";
     }
 }
