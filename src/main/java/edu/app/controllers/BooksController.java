8d70ecc1b5460630f8f053384c617ad22cf4f22c
Alexandr Mizev <mizzzev@outlook.com>
2022-08-18 22:58:19 +0300

    - created newBook view - added new methods in services & book controller for getting all books and creating new book

--- src/main/java/edu/app/controllers/BooksController.java
+++ src/main/java/edu/app/controllers/BooksController.java
@@ -1,15 +1,47 @@
 package edu.app.controllers;
 
+import edu.app.models.Book;
+import edu.app.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
 
+    private final BookService bookService;
+
+    @Autowired
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/")
-    public String getBooksMainPage(){
+    public String getBooksMainPage(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
         return "books/booksMainPage";
+    }
+
+    @GetMapping("/new")
+    public String getNewBookPage(Model model) {
+        model.addAttribute("book", new Book());
+        return "books/newBook";
+    }
+
+    @PutMapping
+    public String addNewBook(@ModelAttribute("book") @Valid Book book, BindingResult error) {
+        if (error.hasErrors()) {
+            return "books/newBook";
+        }
+        bookService.addNewBook(book);
+        return "redirect:/books/";
     }
 }
--- src/main/java/edu/app/services/BookService.java
+++ src/main/java/edu/app/services/BookService.java
@@ -5,6 +5,9 @@ import edu.app.repositories.BookRepositories;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 public class BookService {
@@ -17,6 +20,11 @@ public class BookService {
 
     @Transactional(readOnly = false)
     public void addNewBook(Book newBook) {
+        newBook.setTimeOfCreation(new Date());
         bookRepositories.save(newBook);
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepositories.findAll();
     }
 }
