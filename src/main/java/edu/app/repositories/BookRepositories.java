5f10588c4b04abdc623d9dab66cf17d2e48e6a56
Alexandr Mizev <mizzzev@outlook.com>
2022-08-29 22:01:53 +0300

    - added custom methods

--- src/main/java/edu/app/repositories/BookRepositories.java
+++ src/main/java/edu/app/repositories/BookRepositories.java
@@ -4,6 +4,9 @@ import edu.app.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepositories extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
 }
--- src/main/java/edu/app/repositories/PersonRepositories.java
+++ src/main/java/edu/app/repositories/PersonRepositories.java
@@ -4,6 +4,9 @@ import edu.app.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepositories extends JpaRepository<Person,Integer> {
+    Optional<Person> findByFullName(String fullName);
 }
